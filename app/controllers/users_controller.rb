db95222243a0f33bb8073eb1e010fb552e3591ab
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-04-26 05:29:34 +0000

    recommit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
     @tweets = @user.tweets
     @favorite_tweets = @user.favorite_tweets
   end
+  
+  def follows
+    user = User.find(params:[:id])
+    @users = user.followings
+  end
+  
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tweets  # これを追加
+  has_many :tweets  
   has_many :favorites
   has_many :favorite_tweets, through: :favorites, source: :tweet
+  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+   
+  has_many :followings, through: :active_relationships, source: :follower
+  
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  
+  has_many :followers, through: :passive_relationships, source: :following
+  
+  def followed_by?(user)
+       passive_relationships.find_by(following_id: user.id).present?
+  end
+  
 end
